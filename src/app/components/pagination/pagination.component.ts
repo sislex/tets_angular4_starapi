3cd4a6ad12b486eec2e47fe5c7fa8effc224a62d
Alex <sislex@ya.ru>
2017-08-25 11:35:07 +0300

    fixed pages

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -8,15 +8,34 @@ import {ResourcesService} from '../../services/resources.service';
 })
 export class PaginationComponent implements OnInit {
     public math;
-    constructor(private resourcesService:ResourcesService) {
+    constructor(private resourcesService: ResourcesService) {
         this.math = Math;
     }
 
     ngOnInit() {
     }
 
-    public updateCurrentPage(i) {
-        this.resourcesService.currentPage += i;
+    public updateCurrentPage(data) {
+        if (data === 'next') {
+            if (this.resourcesService.currentPage < this.resourcesService.resActive.obj.results.length / this.resourcesService.col) {
+                this.resourcesService.currentPage += 1;
+            }
+        } else if (data === 'prev') {
+            if (this.resourcesService.currentPage > 1) {
+                this.resourcesService.currentPage -= 1;
+            }
+        } else {
+            this.resourcesService.currentPage = data;
+        }
+    }
+
+    public pagesArr(resurs, rows, col) {
+        const arr = [];
+        const comparison = rows / col;
+        for (let i = 0; i < comparison; i++) {
+            arr[i] =  i + 1;
+        }
+        return arr;
     }
 
 }
